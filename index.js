f6bf8c7a3a764f5da0271d85ccdff58b6aa738d9
ValRar <val21219@gmail.com>
2022-08-22 01:09:36 +0300

    making good commands registering

--- index.js
+++ index.js
@@ -1,56 +1,43 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, messageLink, SlashCommandBuilder, SlashCommandStringOption, SlashCommandUserOption } = require('discord.js');
 const Discordjs = require('discord.js')
-const TOKEN="MTAxMDg4MzMwMzcwMTc0MTU3MA.GiovL5.gm7yb6CatcI3UBj4Tfj6NCukOdynEkqBpj_KOI"
-const GUILD_ID="1010887912541405205"
+const dotenv = require('dotenv').config()
+const fs = require('fs')
+
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
-    const guild = client.guilds.cache.get(GUILD_ID)
+    //const guild = client.guilds.cache.get(GUILD_ID)
 
-    let commands
-    if (guild){
-        commands = guild.commands
-    } else {
-        commands = client.application.commands
-    }
-    commands.create({
-        name: 'ping',
-        description: 'send pong',
-    })
-    commands.create({
-        name: 'sum',
-        description: 'sum of 2 numbers.',
-        options: [
-            {
-                name: 'num1',
-                description: 'the first number',
-                required: true,
-                type: Discordjs.ApplicationCommandOptionType.Number
-            },
-            {
-                name: 'num2',
-                description: 'the second number',
-                required: true,
-                type: Discordjs.ApplicationCommandOptionType.Number
-            }
-        ]
-    })
-    commands.create({
-        name: 'qrcode',
-        description: 'makes a qr code',
-        options: [
-            {
-                name: 'source',
-                description: 'The source in qr code.',
-                required: true,
-                type: Discordjs.ApplicationCommandOptionType.String,
-            },
-        ],
-    })
+    const commands = [
+        new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('send pong'),
+        new SlashCommandBuilder()
+        .setName('qrcode')
+        .setDescription('makes a qr code')
+        .addStringOption(new SlashCommandStringOption()
+            .setName('source')
+            .setDescription('The source in qr code.')
+            .setRequired(true)),
+        new SlashCommandBuilder()
+        .setName('ban')
+        .setDescription('bans a user')
+        .addUserOption(new SlashCommandUserOption()
+            .setName('user')
+            .setDescription('a user to be banned')
+            .setRequired(true)
+        )
+        .addStringOption(
+            new SlashCommandStringOption()
+            .setName('reason')
+            .setDescription('reason of ban')
+            .setRequired(false)
+        )
+    ]
+    client.guilds.cache.each(guild => addCommands(commands, guild.id))
 });
 
 client.on('interactionCreate', async (interaction) => {
@@ -70,19 +57,58 @@ client.on('interactionCreate', async (interaction) => {
             ephemeral: true,
         })
     } else if (commandName === 'qrcode'){
-        const source = options.getString('source')
+        let source = options.getString('source')
         if (source){
+            source = source.replace(/ /gi, '%20')
+            
             interaction.reply({
                 content: `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${source}`,
                 ephemeral: false,
             })
         }
+    } else if (commandName === 'ban'){
+        if (!interaction.member.permissions.has('BAN_MEMBERS')) {
+            interaction.reply({
+                content: 'You don`t have permission to ban members on this server.',
+                ephemeral: true,
+            })
+            return
+        }
+        const user = options.getUser('user')
+        const reason = options.getString('reason')
+        try {
+        interaction.guild.members.ban(user, {
+            reason: reason,
+        })
+        interaction.reply({
+            content: `User @${user.tag} was succesfully permanently banned with reason: ${reason}.`
+        })
+        } catch {
+            interaction.reply({
+                content: "Bot can`t ban this user!",
+                ephemeral: true,
+            })
+        }
     }
 })
 
+function addCommands(commands, guildId) {
+const { SlashCommandBuilder, Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const clientId = '1010883303701741570'
+
+	commands.map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log(`Successfully registered application commands in Guild id - ${guildId}.`))
+	.catch(console.error);
+}
 // client.on('messageCreate', async (message) => {
 //     if (message.author.id != client.user.id) message.channel.send("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example")
 // })
-
+// https://discord.com/api/oauth2/authorize?client_id=1010883303701741570&permissions=8&scope=bot%20applications.commands
+// https://discord.gg/g3RrJHNk
 // Login to Discord with your client's token
-client.login(TOKEN);
+client.login(process.env.TOKEN);
